7be3408fac47eba16ac8df16aeb5e51439aee261
drunktrader <neerajrajputa786@gmail.com>
2026-01-25 11:35:58 +0530

    shifted helper modules to include

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 
-# Change these from relative to absolute imports
+#imports from local files
 from valid_timeframe import validate_timeframe
 from id_generator import IDGenerator
 from download_command import download_tweet_video
@@ -26,8 +26,11 @@ app.add_middleware(
 )
 
 #serve the download director for file access
-os.makedirs("downloads", exist_ok=True)
-app.mount("/downloads", StaticFiles(directory = "downloads"), name="downloads")
+DOWNLOAD_DIR = "'/tmp/downloads"
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+#Mount theh static directory so files can be accessed via URL
+app.mount("/downloads", StaticFiles(directory = DOWNLOAD_DIR), name="downloads")
 
 @app.get("/")
 async def root():
@@ -51,25 +54,30 @@ async def clip_video(request: Request):
     start_sec, end_sec = validate_timeframe(start_raw, end_raw)
 
     # Set up filenames and URLs (base -> localhost for testing, request_id -> unique identifier, video_filename -> downloaded video, clipped_filename -> final clipped video)
-    base_url = os.getenv("BASE_URL", "http://localhost:9000")
     request_id = id_generator.generate_id()
-    video_filename = f"downloads/{request_id}.mp4"
+    video_filename = os.path.join(DOWNLOAD_DIR, f"{request_id}.mp4")
     
-    # Step1 : Download the tweet video using yt-dlp
-    download_tweet_video(tweet_url, video_filename)
+    try:
+        # Step1 : Download the tweet video using yt-dlp
+        download_tweet_video(tweet_url, video_filename)
     
-    # Step 2 : Clip the video using ffmpeg (only if start/end provided)
-    if start_sec is not None and end_sec is not None:
-        clipped_filename = f"downloads/clipped_{request_id}.mp4"
-        clip_video_ffmpeg(video_filename, clipped_filename, start_sec, end_sec)
-        download_link = f"{base_url}/downloads/clipped_{request_id}.mp4"
+        # Step 2 : Clip the video using ffmpeg (only if start/end provided)
+        if start_sec is not None and end_sec is not None:
+            clipped_filename = os.path.join(DOWNLOAD_DIR, f"clipped_{request_id}.mp4")
+            clip_video_ffmpeg(video_filename, clipped_filename, start_sec, end_sec)
+            target_filename = f"clipped_{request_id}.mp4"
     
-    else:
-        # Return full video if no clipping times provided
-        download_link = f"{base_url}/downloads/{request_id}.mp4"
-    
-    # Step 3 : Return the download link
-    return JSONResponse(content={"download_link": download_link})
+        else:
+            # Return full video if no clipping times provided
+            target_filename = f"{request_id}.mp4"
+        # Step 3 : Return the download link
+        base_url = os.getenv("BASE_URL", str(request.base_url).rsplit("/"))
+        download_url = f"{base_url}/downloads/{target_filename}"
+
+        return JSONResponse(content={"download_url": download_url})
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Processing Error: {str(e)}")
 
 if __name__ == "__main__":
     import uvicorn
